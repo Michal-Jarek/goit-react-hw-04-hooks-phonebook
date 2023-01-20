5b2d13313a78d3def79cadae9919a3c70658f5a5
Michał Jarek <miciu77@gmail.com>
2023-01-20 19:36:41 +0100

    Done for today

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import Section from './Section/Section';
@@ -6,37 +6,57 @@ import SignForm from './SignForm/SignForm';
 import UserList from './UserList/UserList';
 import Filter from './Filter/Filter';
 
-const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  filter: '',
-};
+const INITIAL_CONTACTS = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-class App extends Component {
-  state = { ...INITIAL_STATE };
+export const App = () => {
+  const [contacts, handleContacts] = useState([...INITIAL_CONTACTS]);
+  const [filter, setFilter] = useState('');
+  console.log('po deklaracji');
+  console.log(contacts);
 
   // ********************* Methods **************
+  useEffect(() => {
+    console.log('w montażu');
+    if (localStorage.getItem('Contacts') !== null) {
+      console.log('jestem w iF');
+      handleContacts(JSON.parse(localStorage.getItem('Contacts')));
+    }
+    console.log(contacts);
+    console.log('po montażu');
+  }, []);
 
-  componentDidMount() {
-    if (localStorage.getItem('Contacts') !== null)
-      this.setState({ contacts: JSON.parse(localStorage.getItem('Contacts')) });
-  }
+  // componentDidMount() {
+  //   if (localStorage.getItem('Contacts') !== null)
+  //     this.setState({ contacts: JSON.parse(localStorage.getItem('Contacts')) });
+  // }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts)
-      localStorage.setItem('Contacts', JSON.stringify(this.state.contacts));
-  }
+  // useEffect(() => {
+  //   console.log('w update');
+  //   localStorage.setItem('Contacts', JSON.stringify(contacts));
+  //   console.log(contacts);
+  //   console.log('po update');
+  // }, [contacts]);
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts)
+  //     localStorage.setItem('Contacts', JSON.stringify(this.state.contacts));
+  // }
+
+  const handleChange = e => {
+    handleContacts({ [e.target.name]: e.target.value });
   };
 
-  handleSubmit = ({ name, number }) => {
-    const contactCopy = [...this.state.contacts];
+  // handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+
+  const handleSubmit = ({ name, number }) => {
+    const contactCopy = [...contacts];
     if (
       contactCopy.some(
         contact => contact.name.toLowerCase() === name.toLowerCase()
@@ -50,15 +70,12 @@ class App extends Component {
       number: number,
       id: nanoid(),
     });
-    this.setState({
-      contacts: contactCopy,
-      filter: '',
-    });
+    handleContacts([...contactCopy]);
     console.log(`Signed up as: ${name}`);
   };
 
-  handleFilter = (filter, array) => {
-    if (filter.length === 0) return this.state.contacts;
+  const handleFilter = (filter, array) => {
+    if (filter.length === 0) return contacts;
     else {
       const arrayCopy = [];
       for (let a = 0; a < array.length; a++)
@@ -68,46 +85,41 @@ class App extends Component {
       return arrayCopy;
     }
   };
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
+  // handleDelete = id => {
+  //   this.setState(prevState => ({
+  //     contacts: prevState.contacts.filter(contact => contact.id !== id),
+  //   }));
+  // };
 
   // ********************* End Methods **************
 
-  render() {
-    const { filter, contacts } = this.state;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        paddingLeft: '15px',
+        alignItems: 'flex-start',
+        fontSize: 40,
+        color: '#010101',
+        backgroundColor: '#e6e3e3',
+      }}
+    >
+      <Section title="Phonebook">
+        <SignForm handleSubmit={handleSubmit} />
+      </Section>
 
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          paddingLeft: '15px',
-          alignItems: 'flex-start',
-          fontSize: 40,
-          color: '#010101',
-          backgroundColor: '#e6e3e3',
-        }}
-      >
-        <Section title="Phonebook">
-          <SignForm handleSubmit={this.handleSubmit} />
-        </Section>
-
-        <Section title="Contacts">
-          <UserList
-            array={this.handleFilter(filter, contacts)}
-            handleDelete={this.handleDelete}
-          >
-            <Filter filter={filter} handleChange={this.handleChange} />
-          </UserList>
-        </Section>
-      </div>
-    );
-  }
-}
-
-export { App };
+      <Section title="Contacts">
+        <UserList
+          array={contacts}
+          // array={handleFilter(filter, contacts)}
+          // handleDelete={handleDelete}
+        >
+          <Filter filter={filter} handleChange={handleChange} />
+        </UserList>
+      </Section>
+    </div>
+  );
+};
