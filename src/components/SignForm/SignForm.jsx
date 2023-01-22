c556eeb4b343b4b61060da03a2877c162b7af4a5
Michał Jarek <miciu77@gmail.com>
2023-01-22 20:26:39 +0100

    Update SignForm.jsx

--- src/components/SignForm/SignForm.jsx
+++ src/components/SignForm/SignForm.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 
 import scss from './SignForm.module.scss';
 
@@ -8,60 +8,65 @@ const INITIAL_STATE = {
   number: '',
 };
 
-class SignForm extends Component {
-  state = { ...INITIAL_STATE };
+const SignForm = ({handleSubmit}) => {
+  
+  const [contact, handleContact] = useState({...INITIAL_STATE})  
 
   // ************ Methods *****************
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.handleSubmit({ ...this.state });
-    this.reset();
+const  handleSubmitInside = e => {
+  e.preventDefault();
+  console.log(contact);
+    handleSubmit(contact );
+    reset();
   };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+const handleChange = e => {
+  handleContact({
+    ...contact,
+    [e.target.name]: e.target.value,
+  });
+};
 
-  reset = () => this.setState({ ...INITIAL_STATE });
+ const reset = () => handleContact({ ...INITIAL_STATE });
 
   // ************ End Methods *****************
 
-  render() {
-    const { name, number } = this.state;
+  const { name, number } = contact;
 
-    return (
-      <form className={scss.form} onSubmit={this.handleSubmit}>
-        <label className={scss.label}>
-          Name:
-          <input
-            className={scss.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            onChange={this.handleChange}
-            value={name}
-            required
-          />
-        </label>
-        <label className={scss.label}>
-          Number:
-          <input
-            className={scss.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            onChange={this.handleChange}
-            value={number}
-            required
-          />
-        </label>
-        <button className={scss.button} type="submit">Add contact</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={scss.form} onSubmit={handleSubmitInside}>
+      <label className={scss.label}>
+        Name:
+        <input
+          className={scss.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          onChange={handleChange}
+          value={name}
+          required
+        />
+      </label>
+      <label className={scss.label}>
+        Number:
+        <input
+          className={scss.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          onChange={handleChange}
+          value={number}
+          required
+        />
+      </label>
+      <button className={scss.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 SignForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
